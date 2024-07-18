c9282cd261c31edb83a396bacd0abadbda6ae2ee
duan binbin <94447477+codeguts@users.noreply.github.com>
2024-07-18 22:11:12 +0800

    7_18_1

---
+++ test7_18_1/main.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+
+/*int main()
+{
+    int n = 0;
+    scanf("%d",&n);
+    if(n%5 == 0)
+        printf("YES\n");
+    else
+        printf("NO\n");
+    return 0;
+}*/
+
+/*
+
+int main()
+{
+    int n =0;
+    scanf("%d",&n);
+    if(n < 0)
+        printf("%d",1);
+    else if(n == 0)
+    printf("%d",0);
+    else
+        printf("%d",-1);
+}*/
+
+
+
+/*
+int main()
+{
+    int seconds = 0;
+    int h = 0;
+    int m = 0;
+    int s = 0;
+    scanf("%d",&seconds);
+    h = seconds / 3600;
+    m =(seconds % 3600) /60;
+    s =(seconds % 3600 %60);
+    printf("%d %d %d",h,m,s);
+}*/
+
+/*
+int sum(int x,int y)
+{
+    return x+y;
+}
+int main()
+{
+    int a = 10;
+    int b = 13;
+    printf("%d",sum(a,b));
+    return 0;
+}*/
+
+/*
+
+int main()
+{
+    int arr[10]={1,2,3,4,5,6,7,8,9,1};
+    int i = 0;
+    while(i < 10)
+    {
+        printf("%d\n",arr[i]);
+        i++;
+    }
+    return 0;
+}*/
+
+
+/*
+int main()
+{
+
+    //浮点数除法
+    printf("%lf\n",7/2.0);
+    printf("%lf\n",7.0/2.0);
+    printf("%lf\n",7.0/2);
+    return 0;
+
+}*/
+
+//
+/*
+int main()
+{
+
+    printf("%d",sizeof(long));
+    return 0;
+}*/
+
+
+/*
+int main()
+{
+    int a = (int)3.14;
+    printf("%d\n",a);
+    return 0;
+}*/
+
+int main()
+{
+
+    returm 0;
+}
